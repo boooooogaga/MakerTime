cbab254c42c9845d5f52be5382598eabbec8b8c3
boo_ga_ga_ <156234871+boooooogaga@users.noreply.github.com>
2026-06-06 21:21:01 +0300

    Damage\DeathScreen

--- Scripts/CharacterControl.cs
+++ Scripts/CharacterControl.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
+    [SerializeField] private GameManager game;
     public int speed;
+
+    public int damage;
+
+    public int health;
     public int jumpForce;
     public Rigidbody2D rb;
     public SpriteRenderer render;
     bool isMirrored = false;
     private Animator anim;
+
+
+    public bool canMove = true;
+
     
     
     void Start()
@@ -21,10 +30,18 @@ public class CharacterControl : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        Movement();
+    {   
+        if(canMove)
+        {
+            Movement();
+        }
         MirrorRender();
         if(Input.GetMouseButtonDown(0)) Attack();
+
+        if(health <= 0)
+        {
+            StartCoroutine(Death());
+        }
     }
 
     private void MirrorRender()
@@ -61,6 +78,11 @@ public class CharacterControl : MonoBehaviour
         {
             anim.SetBool("Grounded", true);
         }
+        if (other.gameObject.CompareTag("Trap"))
+        {
+            StartCoroutine(spikeDamage(other.gameObject));
+        }
+        
     }
     void OnCollisionExit2D(Collision2D other)
     {
@@ -68,5 +90,33 @@ public class CharacterControl : MonoBehaviour
         {
             anim.SetBool("Grounded", false);
         }
+    }
+
+    private IEnumerator spikeDamage(GameObject spike)
+    {
+        canMove = false;
+        Vector2 knockbackDirection = new Vector2((transform.position.x - spike.transform.position.x), -(transform.position.y - spike.transform.position.y)).normalized;
+        health -= spike.GetComponent<Trap>().damage;
+        rb.AddForce(knockbackDirection * spike.GetComponent<Trap>().dashForce, ForceMode2D.Impulse);
+        for (int i = 0; i < 3; i++)
+        {
+            render.enabled = false;
+            yield return new WaitForSeconds(0.1f);
+            render.enabled = true;
+            yield return new WaitForSeconds(0.1f);
+        }
+        yield return new WaitForSeconds(0.7f);
+        canMove = true;
+    }
+
+    private IEnumerator Death()
+    {
+        anim.SetTrigger("Dead");
+        canMove = false;
+
+        yield return new WaitForSeconds(anim.GetCurrentAnimatorClipInfo(0)[0].clip.length * 2);
+        anim.enabled = false;
+        yield return new WaitForSeconds(1f);
+        game.ShowDeathScreen();
     }
 }
