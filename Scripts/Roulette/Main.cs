0fa7ebe79ec9b3cf2080221755752652e6aca595
boo_ga_ga_ <156234871+boooooogaga@users.noreply.github.com>
2026-06-12 14:49:37 +0300

    roulette anim\logic

--- Scripts/Roulette/Main.cs
+++ Scripts/Roulette/Main.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
+    [SerializeField] private AudioClip[] sounds;
     public bool[] drum = new bool[6];
     public int currentBullet = 0;
+
+    public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         reload();
+        anim = GetComponent<Animator>();
     }
 
     
@@ -17,14 +21,7 @@ public class Main : MonoBehaviour
     {
         if(Input.GetMouseButtonDown(0))
         {
-            if (Shot())
-            {
-                fire();
-            }
-            else
-            {
-                blank();
-            }
+            anim.SetTrigger("Shot");
         }        if(Input.GetMouseButtonDown(2))
         {
             reload();
@@ -34,29 +31,39 @@ public class Main : MonoBehaviour
         }
     }
 
-    public bool Shot()
+    public void Shot()
     {
         if (drum[currentBullet])
         {
-            return true;
-        }
-        else
-        {
+            fire();
+            drum[currentBullet] = false;
             currentBullet++;
             if (currentBullet >= 6)
             {
                 currentBullet = 0;
             }
-            return false;
+        }
+        else
+        {
+            blank();
+            currentBullet++;
+            if (currentBullet >= 6)
+            {
+                currentBullet = 0;
+            }
         }
     }
     public void fire()
-    {
+    {   
         Debug.Log("Fire");
+        anim.ResetTrigger("Fire");
+        anim.SetTrigger("Fire");   
     }
     public void blank()
     {
         Debug.Log("Blank");
+        anim.ResetTrigger("Blank");
+        anim.SetTrigger("Blank");  
     }
     public void reload()
     {
@@ -68,5 +75,9 @@ public class Main : MonoBehaviour
     public void spin()
     {
         currentBullet = Random.Range(0, 6);
+    }
+    public void PlaySound(int num)
+    {
+        AudioSource.PlayClipAtPoint(sounds[num], transform.position, 5f);
     }
 }
